de1e6241cd643726e40d6373b8fa3289b7abea0f
Sue Ann Ioanis <sueannioanis@Sues-MacBook-Pro.local>
2017-01-08 16:50:10 -0500

    Add landing page.

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,20 +1,34 @@
-// This is super crude I'm gonna eventually convert this to a more maintainable framework.
+//  ------
+// This is super crude need to 
+// eventually convert this to a more 
+// maintainable framework - for demo.
+//  ------
 
-$('.sign-in,.sign-up').click(function() {
-  $('.form-signin,.form-signup').addClass('hide');
+var elementsOnPage = '.form-signin, .matches, .landing',
+    navItem        = 'nav li';
+
+function formSubmission() {
+  $('.form-signin, .form-signup, .landing').addClass('hide');
   $('.matches').removeClass('hide');
-});
+}
 
-$('nav li').click(function() {
-  $('nav li').removeClass('active');
+function switchViews() {
+  $(navItem).removeClass('active');
   $(this).addClass('active');
   
-  if ($(this).hasClass('signup')) {
-    $('.form-signin,.matches').addClass('hide');
-    $('.form-signup').removeClass('hide');
-  } else if ($(this).hasClass('login')) {
-    $('.form-signup,.matches').addClass('hide');
-    $('.form-signin').removeClass('hide');
-  }
+  var className = $(this).hasClass('signup') ? 'signup' : 'login';
 
-});
+  switch(className) {
+    case 'signup' : 
+      $(elementsOnPage).addClass('hide');
+      $('.form-signup').removeClass('hide');
+    case 'login' : 
+      $(elementsOnPage).addClass('hide');
+      $('.form-signin').removeClass('hide');
+    default: 
+      break;
+  }
+}
+
+$(navItem).click(switchViews);
+$('.sign-in, .sign-up').click(formSubmission);
